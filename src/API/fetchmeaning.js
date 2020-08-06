4e082cc516565b04872f2665ff317386dc56b5c3
Jaga <kingsmanjagadeesh@gmail.com>
2020-08-06 16:22:00 +0530

    use env varaible for api key

--- src/API/fetchmeaning.js
+++ src/API/fetchmeaning.js
@@ -1,8 +1,8 @@
-import * as apidetails from './apidetails';
 
 async function fetchmeaning(query){
+    const key=process.env.REACT_APP_API_KEY;
     if(query.trim().length !== 0){
-        const response=await fetch("https://www.dictionaryapi.com/api/v3/references/collegiate/json/"+query+"/?key="+apidetails.key);
+        const response=await fetch("https://www.dictionaryapi.com/api/v3/references/collegiate/json/"+query+"/?key="+key);
         const data=await response.json();
         if(typeof data[0] === "object")
         return data[0].shortdef;   
